2a491ec994c9d72f699411c3fc8396bfbe41a0ae
Janoš Orgusaar <janos.orgusaar>
2022-01-24 16:15:17 +0200

    upload

--- src/app.ts
+++ src/app.ts
@@ -23,6 +23,8 @@ export async function openDatabaseConnection() {
   }
   return conn;
 }
+
+openDatabaseConnection();
 App.get('/users', async (req, res) => {});
 App.get('/', (req, res) => {
   res.json({ message: 'Hello!' });
