cdef1c91105855919706e8d5264ed8c1a576ab87
Janoš Orgusaar <janos.orgusaar>
2022-01-24 12:25:21 +0200

    upload

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
-const http = require('http');
-const server = http.createServer((req,res) => {
-    res.statuscode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello');
-}
-)
-
-
-    ;
+const http = require("http");
+const server = http.createServer((req, res) => {
+  res.statuscode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello");
+});
 server.listen(3000, () => {
-    console.log('Server started on http://localhost:3000')
+  console.log("Server started on http://localhost:3000");
 });
