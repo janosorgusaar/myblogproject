5f69b3d7f9d4f53998c0473de5d17e20776182c6
Janoš Orgusaar <janos.orgusaar>
2022-01-24 16:24:40 +0200

    upload

--- src/app.ts
+++ src/app.ts
@@ -7,24 +7,6 @@ import {
 } from 'typeorm';
 const App = express();
 
-export async function openDatabaseConnection() {
-  //await closeDatabaseConnection();
-  const options: ConnectionOptions = {
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'Jompsik1',
-    database: 'blog'
-  };
-  const conn = await createConnection(options);
-  if (conn.isConnected) {
-    throw new Error('Connection to database failed');
-  }
-  return conn;
-}
-
-openDatabaseConnection();
 App.get('/users', async (req, res) => {});
 App.get('/', (req, res) => {
   res.json({ message: 'Hello!' });
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
-import http from 'http';
-
+import { createServer } from 'http';
 import App from './app';
+import { openDatabaseConnection } from './db/db';
 
-const httpServer = http.Server(App);
-
-// const message: string = 'Hello';
-httpServer.listen(3000, () => {
-  console.log('Server started on http://localhost:3000');
-});
+const startServer = async () => {
+  const httpServer = createServer(App);
+  await openDatabaseConnection();
+  httpServer.listen(3000, () => {
+    console.log('Server started on http://localhost:3000');
+  });
+};
+startServer();
