41fd679bcd41e03cc64fd01a14bb2ffc986840fe
Janoš Orgusaar <janos.orgusaar>
2022-01-24 16:09:57 +0200

    upload

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,28 @@
 import express from 'express';
-import { createConnection, Connection, createConnections } from 'typeorm';
+import {
+  createConnection,
+  Connection,
+  createConnections,
+  ConnectionOptions
+} from 'typeorm';
 const App = express();
-const conn = createConnections([
-  {
+
+export async function openDatabaseConnection() {
+  //await closeDatabaseConnection();
+  const options: ConnectionOptions = {
     type: 'mysql',
     host: 'localhost',
     port: 3306,
     username: 'root',
     password: 'Jompsik1',
     database: 'blog'
+  };
+  const conn = await createConnection(options);
+  if (conn.isConnected) {
+    throw new Error('Connection to database failed');
   }
-]);
-
+  return conn;
+}
 App.get('/users', async (req, res) => {});
 App.get('/', (req, res) => {
   res.json({ message: 'Hello!' });
