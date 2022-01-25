6230b40680e008aa5ab598f961171751dce52813
Janos Orgusaar <janos@vau.local>
2022-01-25 11:14:35 +0200

    upload

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -3,7 +3,8 @@ import {
   CreateDateColumn,
   Entity,
   PrimaryGeneratedColumn,
-  UpdateDateColumn
+  UpdateDateColumn,
+  getConnection
 } from 'typeorm';
 
 @Entity()
@@ -12,24 +13,27 @@ export class User {
   id: string;
   @Column('varchar', { length: 150 })
   firstName: string;
-  @Column('varchar', { length: 150 })
-  middleName: string;
+  @Column('varchar', { length: 150, nullable: true })
+  middleName?: string;
   @Column('varchar', { length: 150 })
   lastName: string;
   @Column('varchar')
   mobile: string;
   @Column('varchar', { length: 320, unique: true })
   email: string;
-  @Column()
+  @Column({ default: () => 'CURRENT_TIMESTAMP' })
   lastLogin: Date;
   @Column('tinytext', { nullable: true })
-  intro: string;
+  intro?: string;
   @Column('text', { nullable: true })
-  profile: string;
+  profile?: string;
   @CreateDateColumn()
   createdAt: Date;
   @UpdateDateColumn()
   updatedAt: Date;
+  save() {
+    return getConnection().getRepository('User').save(this);
+  }
 }
 // const myFunction = (input: { isString: boolean }) => {
 //   let variable = input.isString ? 'Hello' : 123;
--- src/routes/user/createUser.ts
+++ src/routes/user/createUser.ts
@@ -1,2 +1,41 @@
-import { Router } from 'express';
-Router;
+import express, { Request, Response } from 'express';
+import { getConnection } from 'typeorm';
+import User from '../../entities/user';
+const router = express.Router();
+
+interface UserInput {
+  firstName: string;
+  middleName?: string;
+  lastName: string;
+  mobile: string;
+  email: string;
+}
+
+router.post('/', async (req: Request, res: Response) => {
+  try {
+    let {
+      firstName,
+      middleName,
+      lastName,
+      mobile,
+      email
+    } = req.body as UserInput;
+
+    const user = new User();
+    user.firstName = firstName;
+    user.middleName = !null ? middleName : '';
+    user.lastName = lastName;
+    user.mobile = mobile;
+    user.email = email;
+
+    let newUser = await user.save();
+    if (!newUser) {
+      throw new Error();
+    }
+    res.send(newUser);
+  } catch (error) {
+    res.send({ message: 'Unable to create new user', message: error.message });
+  }
+
+  return;
+});
