43788612b0ba171f7eb203428bbc3317175172de
Janos Orgusaar <janos@vau.local>
2022-01-25 10:19:56 +0200

    upload

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,6 @@
 import express from 'express';
-import {
-  createConnection,
-  Connection,
-  createConnections,
-  ConnectionOptions
-} from 'typeorm';
 const App = express();
+App.use(express.json());
 
 App.get('/users', async (req, res) => {});
 App.get('/', (req, res) => {
